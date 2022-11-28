8ee86e548f568d3ef8fc366310209de0f4f1dfe3
fendoudebb <15317832520@163.com>
2022-11-28 21:30:22 +0800

    use Java16 record as config class

--- src/main/java/dev/z/blog/config/blog/PortalProperties.java
+++ src/main/java/dev/z/blog/config/blog/PortalProperties.java
@@ -1,11 +1,12 @@
 package dev.z.blog.config.blog;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties(prefix = "blog.portal")
 public record PortalProperties(
-        String title,
-        String author,
-        String keywords,
-        String github) {
+        @DefaultValue("Blog") String title,
+        @DefaultValue("fendoudebb") String author,
+        @DefaultValue("Spring Native, Spring Boot") String keywords,
+        @DefaultValue("https://github.com/fendoudebb/z-blog-spring-native") String github) {
 }
