585de71c68354903d558006d52a86c5adff93b14
fendoudebb <15317832520@163.com>
2023-01-25 11:42:15 +0800

    @Configuration proxyBeanMethods false

--- src/main/java/z/note/lite/config/infra/CacheConfig.java
+++ src/main/java/z/note/lite/config/infra/CacheConfig.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class CacheConfig {
 
     @ConditionalOnProperty(prefix = "infra.cache", name = "type", havingValue = "jdk")
--- src/main/java/z/note/lite/config/infra/LockConfig.java
+++ src/main/java/z/note/lite/config/infra/LockConfig.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class LockConfig {
 
     @ConditionalOnProperty(prefix = "infra.lock", name = "type", havingValue = "jdk")
