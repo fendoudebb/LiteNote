d9bb8f9d147c42389e0ace415ca1fd67f97f2ed0
fendoudebb <15317832520@163.com>
2023-02-04 16:15:03 +0800

    add JFR startup

--- src/main/java/z/note/lite/LiteNoteApplication.java
+++ src/main/java/z/note/lite/LiteNoteApplication.java
@@ -3,13 +3,18 @@ package z.note.lite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.core.metrics.jfr.FlightRecorderApplicationStartup;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class LiteNoteApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LiteNoteApplication.class, args);
+//        SpringApplication.run(LiteNoteApplication.class, args);
+        SpringApplication app = new SpringApplication(LiteNoteApplication.class);
+//        app.setApplicationStartup(new BufferingApplicationStartup(2048));
+        app.setApplicationStartup(new FlightRecorderApplicationStartup());
+        app.run(args);
     }
 
 }
