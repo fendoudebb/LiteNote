2d4664769872495c0c471cc87e5ff61e65d868ca
fendoudebb <15317832520@163.com>
2023-03-30 09:54:12 +0800

    system user repository

--- src/main/java/z/note/lite/repository/api/SysUserRepository.java
+++ src/main/java/z/note/lite/repository/api/SysUserRepository.java
@@ -1,11 +1,13 @@
 package z.note.lite.repository.api;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import z.note.lite.dto.response.SysUserDto;
+import z.note.lite.model.admin.NameOnly;
 import z.note.lite.model.admin.SysUser;
 
 public interface SysUserRepository extends ListCrudRepository<SysUser, Long>, PagingAndSortingRepository<SysUser, Long>, QueryByExampleExecutor<SysUser> {
@@ -13,5 +15,9 @@ public interface SysUserRepository extends ListCrudRepository<SysUser, Long>, Pa
     SysUser findByUsername(String username);
 
     Page<SysUserDto> findAllBy(Pageable pageable);
+
+    Page<NameOnly> getAllBy(Pageable pageable);
+
+    Page<NameOnly> getAllBy(Example<SysUser> probe, Pageable pageable);
 
 }
