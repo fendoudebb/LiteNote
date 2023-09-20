c154019158cc5b92d1802659d709dc483739a76d
fendoudebb <15317832520@163.com>
2023-09-20 09:15:59 +0800

    global error controller

--- src/main/java/z/note/lite/web/controller/GlobalErrorController.java
+++ src/main/java/z/note/lite/web/controller/GlobalErrorController.java
@@ -1,9 +1,9 @@
-package z.note.lite.controller;
+package z.note.lite.web.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import z.note.lite.constant.mvc.Endpoint;
+import z.note.lite.web.Endpoint;
 
 @Controller
 public class GlobalErrorController implements ErrorController {
