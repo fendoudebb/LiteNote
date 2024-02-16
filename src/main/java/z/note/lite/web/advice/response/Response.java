c0db1b1c675fcd1a7f577c632bf221a8f29862d3
fendoudebb <15317832520@163.com>
2024-02-16 21:46:08 +0800

    response

--- src/main/java/z/note/lite/web/advice/response/Response.java
+++ src/main/java/z/note/lite/web/advice/response/Response.java
@@ -3,6 +3,8 @@ package z.note.lite.web.advice.response;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.MDC;
+import z.note.lite.web.filter.trace.TraceFilter;
 
 @Setter
 @Getter
@@ -16,5 +18,13 @@ public class Response {
     private String msg;
 
     private Object data;
+
+    public static Response success(Object data) {
+        return Response.builder().requestId(MDC.get(TraceFilter.TRACE_ID)).data(data).build();
+    }
+
+    public static Response fail(String msg) {
+        return Response.builder().requestId(MDC.get(TraceFilter.TRACE_ID)).code(-1).msg(msg).build();
+    }
 
 }
