2fd751aa78957a746e763dbc8b612dd230ba41cf
fendoudebb <15317832520@163.com>
2024-02-23 10:29:12 +0800

    RestTemplate Config

---
+++ src/main/java/z/note/lite/config/http/RestTemplateConfig.java
@@ -0,0 +1,29 @@
+package z.note.lite.config.http;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setErrorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse response) {
+                return false;
+            }
+
+            @Override
+            public void handleError(ClientHttpResponse response) {
+
+            }
+        });
+        return restTemplate;
+    }
+
+}
