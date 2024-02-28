c210517f5caa29a9526afa58a03122b7d2079ba6
fendoudebb <15317832520@163.com>
2024-02-28 09:18:18 +0800

    id worker

--- src/main/java/z/note/lite/infra/IdWorker.java
+++ src/main/java/z/note/lite/infra/IdWorker.java
@@ -1,5 +1,7 @@
 package z.note.lite.infra;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class IdWorker {
 
     private final long workerId; // 这个就是代表了机器id
@@ -14,13 +16,15 @@ public class IdWorker {
 
     private long lastTimestamp = -1L;
 
-    private final static IdWorker idWorker;
+    private static final ReentrantLock lock = new ReentrantLock();
+
+    private static final IdWorker idWorker;
 
     static {
         idWorker = new IdWorker(1, 1, 1);
     }
 
-    public IdWorker(long workerId, long datacenterId, long sequence) {
+    private IdWorker(long workerId, long datacenterId, long sequence) {
         // sanity check for workerId
         // 这儿不就检查了一下，要求就是你传递进来的机房id和机器id不能超过32，不能小于0
         // 这个是二进制运算，就是5 bit最多只能有31个数字，也就是说机器id最多只能是32以内
@@ -28,7 +32,6 @@ public class IdWorker {
         if (workerId > maxWorkerId || workerId < 0) {
             throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", maxWorkerId));
         }
-
         // 这个是一个意思，就是5 bit最多只能有31个数字，机房id最多只能是32以内
         long maxDatacenterId = ~(-1L << datacenterIdBits);
         if (datacenterId > maxDatacenterId || datacenterId < 0) {
@@ -45,49 +48,47 @@ public class IdWorker {
     }
 
     // 这个是核心方法，通过调用 nextId() 方法，让当前这台机器上的 snowflake 算法程序生成一个全局唯一的 id
-    public synchronized long nextId() {
-        // 这儿就是获取当前时间戳，单位是毫秒
-        long timestamp = timeGen();
-
-        if (timestamp < lastTimestamp) {
-            System.err.printf("clock is moving backwards. Rejecting requests until %d.", lastTimestamp);
-            throw new RuntimeException(String.format("Clock moved backwards. Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
-        }
-
-        // 下面是说假设在同一个毫秒内，又发送了一个请求生成一个 id
-        // 这个时候就得把 sequence 序号给递增 1，最多就是 4096
-        long sequenceBits = 12L;
-        if (lastTimestamp == timestamp) {
-            // 这个意思是说一个毫秒内最多只能有 4096个 数字，无论你传递多少进来，
-            //这个位运算保证始终就是在 4096 这个范围内，避免你自己传递个 sequence 超过了 4096 这个范围
-            long sequenceMask = ~(-1L << sequenceBits);
-            sequence = (sequence + 1) & sequenceMask;
-
-            if (sequence == 0) {
-                timestamp = tilNextMillis(lastTimestamp);
+    private long nextId() {
+        lock.lock();
+        try {
+            // 这儿就是获取当前时间戳，单位是毫秒
+            long timestamp = timeGen();
+            if (timestamp < lastTimestamp) {
+                System.err.printf("clock is moving backwards. Rejecting requests until %d.", lastTimestamp);
+                throw new RuntimeException(String.format("Clock moved backwards. Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
             }
-        } else {
-            sequence = 0;
+            // 下面是说假设在同一个毫秒内，又发送了一个请求生成一个 id
+            // 这个时候就得把 sequence 序号给递增 1，最多就是 4096
+            long sequenceBits = 12L;
+            if (lastTimestamp == timestamp) {
+                // 这个意思是说一个毫秒内最多只能有 4096个 数字，无论你传递多少进来，
+                //这个位运算保证始终就是在 4096 这个范围内，避免你自己传递个 sequence 超过了 4096 这个范围
+                long sequenceMask = ~(-1L << sequenceBits);
+                sequence = (sequence + 1) & sequenceMask;
+                if (sequence == 0) {
+                    timestamp = tilNextMillis(lastTimestamp);
+                }
+            } else {
+                sequence = 0;
+            }
+            // 这儿记录一下最近一次生成id的时间戳，单位是毫秒
+            lastTimestamp = timestamp;
+            // 这儿就是最核心的二进制位运算操作，生成一个 64 bit 的 id
+            // 先将当前时间戳左移，放到 41 bit那儿；将机房 id 左移放到 5 bit 那儿；将机器 id 左移放到 5 bit 那儿；将序号放最后 12 bit
+            // 最后拼接起来成一个 64 bit的二进制数字，转换成 10 进制就是个 long 型
+            long datacenterIdShift = sequenceBits + workerIdBits;
+            long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+            long twepoch = 1608607694000L;
+            return ((timestamp - twepoch) << timestampLeftShift) |
+                    (datacenterId << datacenterIdShift) |
+                    (workerId << sequenceBits) | sequence;
+        } finally {
+            lock.unlock();
         }
-
-        // 这儿记录一下最近一次生成id的时间戳，单位是毫秒
-        lastTimestamp = timestamp;
-
-        // 这儿就是最核心的二进制位运算操作，生成一个 64 bit 的 id
-        // 先将当前时间戳左移，放到 41 bit那儿；将机房 id 左移放到 5 bit 那儿；将机器 id 左移放到 5 bit 那儿；将序号放最后 12 bit
-        // 最后拼接起来成一个 64 bit的二进制数字，转换成 10 进制就是个 long 型
-        long datacenterIdShift = sequenceBits + workerIdBits;
-        long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
-        long twepoch = 1595657220000L;
-        return ((timestamp - twepoch) << timestampLeftShift) |
-                (datacenterId << datacenterIdShift) |
-                (workerId << sequenceBits) | sequence;
     }
 
     private long tilNextMillis(long lastTimestamp) {
-
         long timestamp = timeGen();
-
         while (timestamp <= lastTimestamp) {
             timestamp = timeGen();
         }
