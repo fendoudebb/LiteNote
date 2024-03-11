b3a68ed0f8679a563458d3804bb8b809e392f871
fendoudebb <15317832520@163.com>
2024-03-11 14:38:26 +0800

    ip

---
+++ src/main/java/z/note/lite/task/UnknownIpQuery.java
@@ -0,0 +1,38 @@
+package z.note.lite.task;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import z.note.lite.entity.Ip;
+import z.note.lite.entity.IpUnknown;
+import z.note.lite.response.PageableRes;
+import z.note.lite.service.api.IpMgmtService;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class UnknownIpQuery {
+
+    @Resource
+    IpMgmtService ipMgmtService;
+
+    @SuppressWarnings("unchecked")
+    @Scheduled(fixedRate = 1, timeUnit = TimeUnit.MINUTES)
+    public void queryUnknownIp() {
+        PageableRes pageableRes = ipMgmtService.findAllUnknown(1, 10);
+        if (pageableRes.getCount() == 0) {
+            return;
+        }
+        List<IpUnknown> list = (List<IpUnknown>) pageableRes.getList();
+        list.forEach(ipUnknown -> {
+            Ip ip = ipMgmtService.findByIp(ipUnknown.getIp());
+            if (ip.getAddress() != null) {
+                ipMgmtService.deleteByIp(ip.getAddress());
+            }
+        });
+    }
+
+}
