81c2587ba7ec696eae75cba5b8ebce6aa78786e0
fendoudebb <15317832520@163.com>
2024-04-21 09:02:42 +0800

    check java process

--- check_process.sh
+++ check_process.sh
@@ -2,9 +2,9 @@
 count=`ps aux | grep 'java' | grep -v 'grep' -c`
 if [ $count -eq 0 ]; then
         now=`date +%F\ %T`
-        echo "[$now] Java is offline, try to restart..." >> /app/check_java.log
-        nohup java -jar /app/lite-note-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >/dev/null 2>&1 &
+        echo "[$now] Java is offline, try to restart..." >> /app/check_process.log
+        nohup /root/.sdkman/candidates/java/21.0.2-amzn/bin/java -jar /app/lite-note-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >/dev/null 2>&1 &
 else
         now=`date +%F\ %T`
-        echo "[$now] Java is online, everything seems to be OK..." >> /app/check_java.log
+        echo "[$now] Java is online, everything seems to be OK..." >> /app/check_process.log
 fi
