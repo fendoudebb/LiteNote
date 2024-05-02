9e6266c21e5e9bca4e7e82f25a130ce4f4b04244
fendoudebb <15317832520@163.com>
2024-05-02 09:05:15 +0800

    tool method

--- src/main/java/z/note/lite/infra/RequestUtils.java
+++ src/main/java/z/note/lite/infra/RequestUtils.java
@@ -7,24 +7,17 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 public interface RequestUtils {
 
+    static HttpServletRequest getHttpServletRequest() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        return attributes.getRequest();
+    }
+
     static String getIp() {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
         String ip = request.getHeader("X-Forwarded-For");
         if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("X-Real-IP");
-        }
-        if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
         }
         if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
@@ -35,10 +28,7 @@ public interface RequestUtils {
     static String getScheme() {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-        String scheme = request.getHeader("X-Forwarded-Proto");
-        if (!StringUtils.hasText(scheme)) {
-            scheme = request.getHeader("X-Forwarded-Scheme");
-        }
+        String scheme = request.getHeader("X-Scheme");
         if (!StringUtils.hasText(scheme)) {
             scheme = request.getScheme();
         }
@@ -48,9 +38,9 @@ public interface RequestUtils {
     static String getHost() {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-        String host = request.getHeader("X-Forwarded-Host");
+        String host = request.getHeader("Host");
         if (!StringUtils.hasText(host)) {
-            host = request.getHeader("Host");
+            host = request.getHeader("X-Forwarded-Host");
         }
         if (!StringUtils.hasText(host)) {
             host = request.getServerName();
