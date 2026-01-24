72359d0d2e6c159f513c2ccf9c3795b88f6a4124
fendoudebb <15317832520@163.com>
2026-01-24 16:25:23 +0800

    stats

---
+++ src/main/java/z/note/lite/entity/PostHourlyStats.java
@@ -0,0 +1,16 @@
+package z.note.lite.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class PostHourlyStats {
+
+    private Integer hour;
+
+    private Integer count;
+
+}
