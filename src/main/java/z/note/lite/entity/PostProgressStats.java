4b4d81fbad17996bddb16415cdfa69ac87e3573d
fendoudebb <15317832520@163.com>
2026-01-26 06:19:29 +0800

    stats

---
+++ src/main/java/z/note/lite/entity/PostProgressStats.java
@@ -0,0 +1,18 @@
+package z.note.lite.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class PostProgressStats {
+
+    private Integer yearCount;
+
+    private Integer monthCount;
+
+    private Integer dayCount;
+
+}
